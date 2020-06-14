69981adcc95c232bdadc8be195a625a0ab21d54f
afrentescu <adelafrentescu@gmail.com>
2020-06-14 12:03:52 +0300

    layout diner restaurants

--- app/src/main/java/com/example/orderit/DinerActivity.java
+++ app/src/main/java/com/example/orderit/DinerActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,7 +26,7 @@ import com.google.android.material.navigation.NavigationView;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class DinerActivity extends AppCompatActivity {
+public class DinerActivity extends AppCompatActivity implements View.OnClickListener {
   EditText etSearch;
     int[]  IMAGES ={ R.drawable.diner2_noeme, R.drawable.diner3_haute, R.drawable.diner4_pio, R.drawable.diner_5_cucinetta, R.drawable.diner_6_kane };
     String[]  NAMES = {"NOEME", "HAUTE", "PIO", "CUCINETTA", "KANE"};
@@ -45,15 +46,51 @@ public class DinerActivity extends AppCompatActivity {
 
         colorDrawable = new ColorDrawable(Color.parseColor("#00995c"));
         bar.setBackgroundDrawable(colorDrawable);
-        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        dl = (DrawerLayout)findViewById(R.id.diner_layout);
         t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.nav_no_smoke:
+                        Toast.makeText(DinerActivity.this, "Smoke Free PLaces",Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_restaurant:
+                        Toast.makeText(DinerActivity.this, "Restaurants",Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_share:
+                        Toast.makeText(DinerActivity.this, "Share",Toast.LENGTH_SHORT).show();break;
+                    default:
+                        return true;
+                }
 
 
+                return true;
+
+            }
+        });
         DinerAdapter customAdapter = new DinerAdapter(this, NAMES, IMAGES);
         recycleViesDiner.setAdapter(customAdapter);
         recycleViesDiner.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
