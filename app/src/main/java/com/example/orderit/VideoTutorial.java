fd847b766b9bbfa2ec7c7e7da6f8964fabdfc97e
Valentina Varzaru <venillavip@gmail.com>
2020-06-22 19:53:34 +0300

    Initial video button added

--- app/src/main/java/com/example/orderit/VideoTutorial.java
+++ app/src/main/java/com/example/orderit/VideoTutorial.java
@@ -1,18 +1,24 @@
 package com.example.orderit;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoTutorial extends AppCompatActivity {
     ColorDrawable colorDrawable;
     ActionBar bar;
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +28,43 @@ public class VideoTutorial extends AppCompatActivity {
         colorDrawable = new ColorDrawable(Color.parseColor("#00995c"));
         bar.setBackgroundDrawable(colorDrawable);
 
-        VideoView videoView = findViewById(R.id.video_view);
+        final VideoView videoView = findViewById(R.id.video_view);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.tutorial1;
-        Uri uri = Uri.parse(videoPath);
+        final Uri uri = Uri.parse(videoPath);
         videoView.setVideoURI(uri);
-        MediaController mediaController = new MediaController(this);
+        final MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
+         //Setup a play button to start the video
+        final ImageButton mPlayButton = (ImageButton)findViewById(R.id.play_button);
+        final MediaPlayer.OnInfoListener onInfoToPlayStateListener = new MediaPlayer.OnInfoListener() {
+
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START) {
+                    mPlayButton.setVisibility(View.GONE);
+                    return true;
+                }
+                return false;
+            }
+
+        };
+
+        videoView.setOnInfoListener(onInfoToPlayStateListener);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(videoView.isPlaying()) {
+                    mPlayButton.setVisibility(View.GONE);
+                }
+                else {
+                    videoView.start();
+                    // show the media controls
+                    mediaController.show();
+                    // hide button once playback starts
+                    mPlayButton.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
